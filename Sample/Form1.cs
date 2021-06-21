c661053603bfa9a5ccac856140c86f62343f313f
kuzbyt <kuzbyt@gmail.com>
2021-06-21 14:15:53 +0300

    Добавьте файлы проекта.

---
+++ Sample/Form1.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Sample.Controller;
+
+namespace Sample
+{
+	public partial class Form1 : Form
+	{
+		Query controler;
+		public Form1()
+		{
+			InitializeComponent();
+			controler = new Query(ConnectionString.ConnStr);
+		}
+
+		private void button1_Click(object sender, EventArgs e)
+		{
+			dataGridView1.DataSource = controler.updatePerson();
+		}
+
+		private void button2_Click(object sender, EventArgs e)
+		{
+			controler.Add(textBox1.Text, textBox2.Text, int.Parse(textBox3.Text));
+		}
+
+		private void button3_Click(object sender, EventArgs e)
+		{
+			controler.Delete(int.Parse(dataGridView1.Rows[dataGridView1.CurrentRow.Index].Cells["ID"].Value.ToString()));
+		}
+	}
+}
---
+++ Sample/Query.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.OleDb;
+using System.Data;
+
+namespace Sample
+{
+	class Query
+	{
+		OleDbConnection connection;
+		OleDbCommand command;
+		OleDbDataAdapter dataAdapter;
+		DataTable bufferTable;
+
+		public Query(string Conn)//підключення
+		{
+			connection = new OleDbConnection(Conn);
+			bufferTable = new DataTable();
+
+		}
+
+		public DataTable updatePerson()//оновлення БД
+		{
+			connection.Open();
+			dataAdapter = new OleDbDataAdapter("SELECT * FROM Person", connection);
+			bufferTable.Clear();
+			dataAdapter.Fill(bufferTable);
+			connection.Close();
+			return bufferTable;
+		}
+
+		public void Add(string FirstName, string LastName, int Age)
+		{
+			connection.Open();
+			command = new OleDbCommand($"INSERT INTO Person(FirstName, LastName, Age) VALUES(@FirstName, @LastName, @Age)", connection);
+			command.Parameters.AddWithValue("FirstName", FirstName);
+			command.Parameters.AddWithValue("LastName", LastName);
+			command.Parameters.AddWithValue("Age", Age);
+			command.ExecuteNonQuery();
+			connection.Close();
+
+		}
+
+		public void Delete(int ID)
+		{
+			connection.Open();
+			command = new OleDbCommand($"DELETE FROM Person Where ID={ID}",connection);
+			command.ExecuteNonQuery();
+			connection.Close();
+		}
+
+	}
+}
